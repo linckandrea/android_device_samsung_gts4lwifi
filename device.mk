c472de7510f6bbf78598206ba6f11a13b586b131
linckandrea <linckandrea01@gmail.com>
2025-02-28 04:08:24 +0100

    t830: remove unused blobs & compile some modules from source

--- device.mk
+++ device.mk
@@ -10,11 +10,17 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
 # API levels
 PRODUCT_SHIPPING_API_LEVEL := 27
 
-# Health
+# DRM
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.1-impl \
-    android.hardware.health@2.1-impl.recovery \
-    android.hardware.health@2.1-service
+    android.hardware.drm@1.0-impl \
+    android.hardware.drm@1.0-service \
+    android.hardware.drm@1.2-service.clearkey \
+    android.hardware.drm@1.2-service.widevine
+
+# Gatekeeper HAL
+PRODUCT_PACKAGES += \
+    android.hardware.gatekeeper@1.0-impl \
+    android.hardware.gatekeeper@1.0-service
 
 # Overlays
 PRODUCT_ENFORCE_RRO_TARGETS := *
