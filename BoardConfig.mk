48509176db5036a61a710d79e3b62be6d1bc05c3
linckandrea <linckandrea01@gmail.com>
2025-03-05 09:10:05 +0100

    add configfs
     * from gts4lv-common(lineage-17.1 branch)

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -133,6 +133,7 @@ BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
 
 # VINTF
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
+TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/config.fs
 
 # Inherit the proprietary files
 include vendor/samsung/gts4lwifi/BoardConfigVendor.mk
